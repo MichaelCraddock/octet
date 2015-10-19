72809590525840494fcba09645e59f4584164f04
MichaelCraddock <MichaelGCraddock@hotmail.co.uk>
2015-10-19 14:50:55 +0100

    Added mouse controlled camera
    Added a camera that can be controlled via the mouse, put the rotating
    box back in to make sure it worked.

--- octet/src/examples/MichaelCraddockswork/MichaelCraddockswork.h
+++ octet/src/examples/MichaelCraddockswork/MichaelCraddockswork.h
@@ -49,11 +49,39 @@ namespace octet {
 
 
 	}
+
+	void Camera(){
+
+		int vx = 0,vy = 0;
+		int x, y;
+		get_viewport_size(vx, vy);
+		get_mouse_pos(x, y);
+
+		mat4t modeltoworld;
+
+		modeltoworld.loadIdentity();
+		modeltoworld.translate(-25.0f, 12.5f, -5.0f);
+		modeltoworld.rotateY((float)-x*2.0f);
+		if (vy / 2 - y < 40 && vy / 2 - y > -40)
+			modeltoworld.rotateX(vy / 2 - y);
+		else if (vy / 2 - y >= 40)
+			modeltoworld.rotateX(40);
+		else if (vy / 2 - y <= 40)
+			modeltoworld.rotateX(-40);
+
+		app_scene->get_camera_instance(0)->get_node()->access_nodeToParent() = modeltoworld;
+	}
   
     void app_init() {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
+	  material *red = new material(vec4(1, 0, 0, 1));
+	  mesh_box *box = new mesh_box(vec3(4));
+	  scene_node *node = new scene_node();
+	  app_scene->add_child(node);
+	  app_scene->add_mesh_instance(new mesh_instance(node, box, red));
 	  loadlevel();
+	
     }
 
   
@@ -66,6 +94,10 @@ namespace octet {
 
       // draw the scene
       app_scene->render((float)vx / vy);
+	  scene_node *node = app_scene->get_mesh_instance(0)->get_node();
+	  node->rotate(1, vec3(1, 0, 0));
+	  node->rotate(1, vec3(0, 1, 0));
+	  Camera();
     }
   };
 }
